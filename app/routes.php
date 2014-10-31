37f63425de349b57655e5abe638f863e6aee4dfd
cjp <No5stranger@gmail.com>
2014-10-31 11:29:57 +0000

    skim laravel #Getting Started# tour

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,61 @@
 |
 */
 
+Route::pattern('id', '[0-9]+');
+
+Route::filter('old', function()
+    {
+        if (Input::get('age') < 20)
+        {
+            return "Can't pass filter";
+        }
+    });
+
 Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('hello', 'HomeController@showWelcome');
+
+Route::post('post', function()
+    {
+        return 'This is a post method';
+    });
+
+Route::match(array('GET', "POST"), 'match', function()
+    {
+        return 'This router match GET and POST method';
+    });
+
+Route::get('id/{id}', function($id)
+    {
+        return 'Hello ' . $id;
+    });
+
+Route::get('user/{name?}', function($name = 'cjp')
+    {
+        return 'Hello ' . $name;
+    })
+    ->where('name', '[a-zA-Z]+');
+
+Route::get('age', array('before' => 'old', function()
+        {
+            return 'You are too old';
+        }));
+
+Route::get('/routed/named', array('as' => 'named', function()
+        {
+            return 'this routes has a alias name';
+        }));
+
+Route::group(array('before' => 'old'), function()
+        {
+            Route::get('/a', function()
+            {
+            });
+
+            Route::get('/b', function()
+            {
+            });
+        });
